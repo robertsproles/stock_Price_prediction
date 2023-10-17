03ed65cdf0dc3e06fd20e3d26b800e27481f2feb
robertsproles <147991822+robertsproles@users.noreply.github.com>
2023-10-17 03:51:18 +0000

    Updated with stacked bar chart

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import yfinance as yf
 import datetime
 from datetime import date
 import altair as alt
-import matplotlib.pyplot as plt
 
 
 header = st.container()
@@ -39,11 +38,19 @@ for i in range(0, len(df)):
 with header:
     st.title('Portfolio Value Chart')
     st.write('Enter the stock (ticker symbols) and number of shares in the sidebar to update the portfolio.')
-#    st.write(df[['Ticker','NumShares']])
     
 with portfolio_chart:
-    st.header('Performance Chart')
-#    st.write(chart_data)
+    st.header('', divider='rainbow')
+    bar_chart_data = pd.DataFrame()
+    line_chart_data = pd.DataFrame()
     for s in range(0, len(df)):
         chart_data = yf.download(df.loc[s,"Ticker"], start_date, end_date)
-        st.line_chart(chart_data, y=["Close"])
+        line_chart_data.insert(0,df.loc[s,"Ticker"],chart_data.loc[:,'Close'],True)
+        chart_data['Close'] = chart_data['Close'].astype(float)
+        chart_data.loc[:,'Close'] = (chart_data.loc[:,'Close'] * df.loc[s,'NumShares'])
+        bar_chart_data.insert(0,df.loc[s,"Ticker"],chart_data.loc[:,'Close'],True)
+    st.write('Individual Performance Over Last', days_to_plot, 'Days ($)')
+    st.line_chart(data = line_chart_data)
+    st.write('')
+    st.write('Portfolio Performance Over Last', days_to_plot, 'Days ($)')
+    st.bar_chart(data = bar_chart_data)
